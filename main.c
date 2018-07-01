397a8b11b3b75c2be6677bd280fd686f49fed703
Dylan Sotomayor <dysotoma@e1z3r4p9.42.us.org>
2018-07-01 16:38:32 -0700

    test suit added

--- main.c
+++ main.c
@@ -20,9 +20,21 @@ int	main(int ac,  char **av)
 
 	if (ac)
 	{
-		printf("\nbytes returned = %i\n: ", ft_printf("%05d", 2147483647));
+		ft_printf("%%-5.3s LYDI == |%-5.3s|\n", "LYDI");
+		ft_printf("%% 4.5i 42 == |% 4.5i|\n", 42);
+		ft_printf("%%04.5i 42 == |%04.5i|\n", 42);
+		ft_printf("%%04.3i 42 == |%04.3i|\n", 42);
+		ft_printf("%%04.2i 42 == |%04.2i|\n", 42);
+
+		printf("%%-5.3s LYDI == |%-5.3s|\n", "LYDI");
+		printf("%% 4.5i 42 == |% 4.5i|\n", 42);
+		printf("%%04.5i 42 == |%04.5i|\n", 42);
+		printf("%%04.3i 42 == |%04.3i|\n", 42);
+		printf("%%04.2i 42 == |%04.2i|\n", 42);
+
+		printf("\nbytes returned = %i\n: ", ft_printf("mine = %d", 0));
 		printf("real printf = |%-7.3o|", 42);
-		printf("%05d", 2147483647);
+		printf("%0.12d", 2147483647);
 		//need to handle %5%
 	}
 }
